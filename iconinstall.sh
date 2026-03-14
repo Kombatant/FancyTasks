6f09444304778756b15322169da2f337b4a8884f
Pete Vagiakos <github@kombatant.com>
2026-03-14 14:34:00 -0400

    fix: Update icon installation script and metadata for new project structure

--- iconinstall.sh
+++ iconinstall.sh
@@ -5,3 +5,4 @@ SCRIPT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 
 mkdir -p "$HOME/.local/share/icons/hicolor/256x256/apps"
 cp "$SCRIPT_DIR/package/FancyTasks.png" "$HOME/.local/share/icons/hicolor/256x256/apps/FancyTasks.png"
+cp "$SCRIPT_DIR/package/FancyTasks.png" "$HOME/.local/share/icons/hicolor/256x256/apps/org.kombatant.fancytasks.png"
